43a1d1f9e94428e7f67145144c04407692ccd0dd
Alberto Pluecker <albertopluecker@gmail.com>
2021-02-27 12:44:49 +0100

    - Now works with integers instead of doubles

--- CalcFunctions.java
+++ CalcFunctions.java
@@ -1,42 +1,42 @@
 public class CalcFunctions(){
 
-    public static double Round(double n1){
+    public static int Round(int n1){
         return Math.round(n1 * 100.0) / 100.0;
     }
 
-    public static double Add(double n1, double n2) {
+    public static int Add(int n1, int n2) {
         return (n1 + n2);
     }
 
-    public static double Subtract(double n1, double n2){
+    public static int Subtract(int n1, int n2){
         return (n1 - n2);
     }
 
-    public static double Multiply(double n1, double n2){
+    public static int Multiply(int n1, int n2){
         return (n1 * n2);
     }
 
-    public static double Divide(double n1, double n2){
+    public static int Divide(int n1, int n2){
         return (n1 / n2);
     }
 
-    public static double Convert(){
+    public static int Convert(){
         System.out.println("Select [1] to convert from C° to F°:\n");
         System.out.println("Select [2] to convert from F° to C°:\n");
-        int conversionOption = input.nextdouble();
+        int conversionOption = input.nextInt();
 
         System.out.println("Type in the value to be converted:");
-        double conversionValue = input.nextdouble();
+        int conversionValue = input.nextInt();
 
 
         switch(conversionOption){
             case 1:
-                double tempF = conversionValue * (9/5) + 32;
+                int tempF = conversionValue * (9/5) + 32;
                 return tempF;
                 break;
 
             case 2:
-                double tempC = (conversionValue - 32) / 18;
+                int tempC = (conversionValue - 32) / 18;
                 return tempC;
                 break;
 
