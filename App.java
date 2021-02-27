7e7ebb4b58ae2ecd3e4e38ec9163e6afee5b734d
Alberto Pluecker <albertopluecker@gmail.com>
2021-02-27 10:43:13 +0100

    - Moved calculator functions to a separate file - Started adding user input validation

--- App.java
+++ App.java
@@ -7,9 +7,6 @@
             5. wipe or save result
         */
 
-        /*TEST PUSH*/
-
-
 import java.util.Scanner;
 
 public class App {
@@ -17,65 +14,27 @@ public class App {
     public static void main(String[] args){
         System.out.println("Simple Calculator \n by albertofp");
         System.out.println("\n\n");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the number of the function you want to use\n");
+        Scanner input = new Scanner(System.in);
+
         System.out.println("[1] Addition \n");
         System.out.println("[2] Subtraction \n");
         System.out.println("[3] Multiplication \n");
         System.out.println("[4] Division \n");
         System.out.println("[5] Celsius/Fahrenheit converter \n");
-        System.out.println("[Q] Exit calculator \n");
+        System.out.println("[6] Exit calculator \n");
+        int functionSelection = input.nextInt();
 
-        int functionSelection = scanner.nextInt();
-        System.out.println(functionSelection);
+        do {
+            System.out.println("Select the number of the function you want to use\n");
+
+            
+            while((!input.hasNextInt()) || functionSelection > 6 || functionSelection < 0 ) {
+                System.out.println("Error! Please select one of the options above.");
+                input.next();
+            }
+        } while (functionSelection <= 0);
+        System.out.println(functionSelection); //for debugging only
+
     }
 
-}
-
-public class Functions{
-
-    public static double Round(double n1){
-        return Math.round(n1 * 100.0) / 100.0;
-    }
-
-    public static double Add(double n1, double n2) {
-        return (n1 + n2);
-    }
-
-    public static double Subtract(double n1, double n2){
-        return (n1 - n2);
-    }
-
-    public static double Multiply(double n1, double n2){
-        return (n1 * n2);
-    }
-
-    public static double Divide(double n1, double n2){
-        return (n1 / n2);
-    }
-
-    public static double Convert(){
-        System.out.println("Select [1] to convert from C° to F°:\n");
-        System.out.println("Select [2] to convert from F° to C°:\n");
-        int conversionOption = scanner.nextInt();
-
-        System.out.println("Type in the value to be converted:");
-        double conversionValue = scanner.nextdouble();
-
-
-        switch(conversionOption){
-            case 1:
-                double tempF = conversionValue * (9/5) + 32;
-                return tempF;
-                break;
-
-            case 2:
-                double tempC = (conversionValue - 32) / 18;
-                return tempC;
-                break;
-
-            default:
-                System.out.println("Error! Select options 1 or 2.");
-        }
-    }
 }
---
+++ CalcFunctions.java
@@ -0,0 +1,48 @@
+public class CalcFunctions(){
+
+    public static double Round(double n1){
+        return Math.round(n1 * 100.0) / 100.0;
+    }
+
+    public static double Add(double n1, double n2) {
+        return (n1 + n2);
+    }
+
+    public static double Subtract(double n1, double n2){
+        return (n1 - n2);
+    }
+
+    public static double Multiply(double n1, double n2){
+        return (n1 * n2);
+    }
+
+    public static double Divide(double n1, double n2){
+        return (n1 / n2);
+    }
+
+    public static double Convert(){
+        System.out.println("Select [1] to convert from C° to F°:\n");
+        System.out.println("Select [2] to convert from F° to C°:\n");
+        int conversionOption = input.nextdouble();
+
+        System.out.println("Type in the value to be converted:");
+        double conversionValue = input.nextdouble();
+
+
+        switch(conversionOption){
+            case 1:
+                double tempF = conversionValue * (9/5) + 32;
+                return tempF;
+                break;
+
+            case 2:
+                double tempC = (conversionValue - 32) / 18;
+                return tempC;
+                break;
+
+            default:
+                System.out.println("Error! Select options 1 or 2.");
+        }
+    }
+}
+
